72af8ca933049d1fe4a67c2abfdedb4fe86fdc55
Abser yousuf <abseryousuf50@gmail.com>
2026-03-05 16:09:59 +0530

    Updated Db js

--- Databse.js
+++ Databse.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const url = 'mongodb+srv://inotebook:<Inotebook23>@inotebook.idrdgre.mongodb.net/?appName=inotebook'
+const url = 'mongodb+srv://inotebook:Inotebook23@inotebook.idrdgre.mongodb.net/?appName=inotebook'
 const Connect = async () => {
     try {
         await mongoose.connect(url)
