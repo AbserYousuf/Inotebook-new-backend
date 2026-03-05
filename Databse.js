fc335a5b49a1b0d87f630ba9445ecc9ac9d00202
Abser yousuf <abseryousuf50@gmail.com>
2026-03-05 16:04:37 +0530

    Update MongoDB connection URL to use Atlas

--- Databse.js
+++ Databse.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const url = 'mongodb://localhost:27017/Notebook'
+const url = 'mongodb+srv://inotebook:<Inotebook23>@inotebook.idrdgre.mongodb.net/?appName=inotebook'
 const Connect = async () => {
     try {
         await mongoose.connect(url)
