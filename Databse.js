997ecb888f502106b6daa508cf646e4d34edee1c
Abser yousuf <abseryousuf50@gmail.com>
2026-03-18 17:55:36 +0530

    Update Databse.js

--- Databse.js
+++ Databse.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const url = 'mongodb+srv://inotebook:Inotebook23@inotebook.idrdgre.mongodb.net/?appName=inotebook'
+const url = process.env.MONGO_URL
 const Connect = async () => {
     try {
         await mongoose.connect(url)
